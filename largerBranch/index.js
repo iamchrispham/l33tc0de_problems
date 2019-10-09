cd43dfe0529425b2be92a0830036ee7577d9abe4
Chris <chrismpham@gmail.com>
2019-10-09 00:55:08 -0700

    larger branch

--- largerBranch/index.js
+++ largerBranch/index.js
@@ -1,66 +1,59 @@
- class Node {
-  constructor(data) {
-    this.data = data;
-    this.left = null;
-    this.right = null;
-  }
+class Node {
+	constructor(data) {
+		this.data = data;
+		this.left = null;
+		this.right = null;
+	}
 };
 
 class BST {
-  constructor() {
-    this.root = null;
-  }
+	constructor() {
+		this.root = null;
+		this.sumLeft = 0;
+		this.sumRight = 0;
+	}
 
-insert(data) 
-{ 
-
-	var newNode = new Node(data); 
-					
-	if(this.root === null) 
-		this.root = newNode; 
-	else
-		this.insertNode(this.root, newNode); 
-} 
-
-insertNode(node, newNode) 
-{ 
-	// if the data is less than the node 
-	// data move left of the tree 
-	if(newNode.data < node.data) 
-	{ 
-		// if left is null insert node here 
-		if(node.left === null) 
-			node.left = newNode; 
+	insert(data) {
+		var newNode = new Node(data);
+		if (this.root === null)
+			this.root = newNode;
 		else
+			this.insertNode(this.root, newNode);
+	}
 
-			// if left is not null recurr until 
-			// null is found 
-			this.insertNode(node.left, newNode); 
-	} 
-
-	// if the data is more than the node 
-	// data move right of the tree 
-	else
-	{ 
-		// if right is null insert node here 
-		if(node.right === null) 
-			node.right = newNode; 
-		else
-
-			// if right is not null recurr until 
-			// null is found 
-			this.insertNode(node.right,newNode); 
-	} 
-} 
+	insertNode(node, newNode) {
+		if (newNode.data < node.data) {
+			if (node.right === null)
+				node.right = newNode;
+			else
+				this.insertNode(node.right, newNode);
+		}
+		else {
+			if (node.left === null)
+				node.left = newNode;
+			else
+				this.insertNode(node.left, newNode);
+		}
+	}
 }
 
 
 const solution = (arr) => {
-  // Type your solution here 
+	// Type your solution here 
 	var tree = new BST();
-	tree.insert(arr[0]);
+	for (var i = 0; i < arr.length; i++) {
+		if (arr[i] !== -1) {
+			console.log('inserting: ', arr[i]);
+			tree.insert(arr[i]);
+		}
+	}
 	console.log(tree);
 };
+/*
+			3
+		6	  2
+	9	 10		9
+*/
 
 
-console.log(solution([3,6,2,9,-1,10]));
+console.log(solution([3, 6, 2, 9, -1, 10]));
