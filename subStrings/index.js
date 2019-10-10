45ff593d0c1619ae850f6498ce76fc97ffefcea7
Chris <chrismpham@gmail.com>
2019-10-10 02:00:31 -0700

    returns first and last substrings

--- subStrings/index.js
+++ subStrings/index.js
@@ -3,9 +3,9 @@
 const vowels = new Set(['a', 'e', 'i', 'o', 'u'])
 const consonants = new Set(['b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'p', 'q', 'r', 's', 't', 'v', 'w', 'x', 'y', 'z'])
 function subStrings(str) {
-  if (str.length < 3) {
+  if (str.length < 3 || str.length > (5 * 10 ^ 5)) {
     return '';
-  } 
+  }
 
   var result = [];
   // write permutations of string
@@ -13,22 +13,24 @@ function subStrings(str) {
     var substr = '';
     for (var j = i; j < str.length; j++) {
       substr += str[j];
-      if(result.indexOf(substr) === -1) result.push(substr);
+      if (result.indexOf(substr) === -1) result.push(substr);
     }
   }
 
-  return result
+  result = result
     .reduce((acc, substr) => {
-      if (vowels.has(substr[0]) && consonants.has(substr[substr.length-1])) {
+      if (vowels.has(substr[0]) && consonants.has(substr[substr.length - 1])) {
         acc.push(substr)
       }
       return acc;
-    }, [])[0] || '';
+    }, []) || '';
+  result = result[0] + "\n" + result[result.length-1];
+  return result;
 }
 
 var ex1 = 'abc'; // ab
 var ex2 = 'aba'; // ab
-var ex3 = 'aaa'; // ''
+var ex3 = 'aaaabbaa'; // ''
 
 console.log(subStrings(ex1));
 console.log(subStrings(ex2));
